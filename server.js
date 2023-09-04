afbdb37c95d40182a491aa3047135b08e660ffa0
varunraj <tuduruvarunraj@gmail.com>
2023-09-04 11:54:13 +0530

    add all

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+
+io.on('connection', (socket) => {
+    console.log('connected');
+  
+    socket.on('message', (data) => {
+      console.log('Received message:', data);
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
+    });
+  });
+  
